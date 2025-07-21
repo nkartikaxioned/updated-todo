1cb19e01fc08f8d1bad0afde381db6c15517db27
dshirlyaxioned <shirlyd@axioned.com>
2025-07-21 19:02:32 +0530

    feat: implementing useReducer hook

--- src/todo/index.jsx
+++ src/todo/index.jsx
@@ -1,53 +1,73 @@
-import { createContext, use, useEffect, useRef, useState } from "react";
+import { createContext, use, useEffect, useReducer, useRef, useState } from "react";
 
 export const TodoContext = createContext();
 export const TodoDataProvider = ({ children }) => {
-  const localKey = "Todo-App";
-  const date = new Date();
 
+  const localKey = "Todo-App";
+  const initialState = {
+    todoData: () => {
+      const storedTodo = localStorage.getItem(localKey);
+      return storedTodo ? JSON.parse(storedTodo) : [];
+    },
+    displayInput: false,
+    editTodoValue: "",
+    filteredTodoData: [],
+    filterValue: "all",
+    toggleDarkMode: "light",
+    emptyTodoMessage: "So peaceful... Add a task to bring some action 💡.",
+    emptyCheckedMessage: "🕵️ Nothing to see here. Try changing the filter!"
+  }
+
+  const reducer = (state, action) => {
+
+  }
+
+  const date = new Date();
+  const currentDate = date.toLocaleDateString();
+  const userInputRef = useRef(null);
+  
   const time = date.toLocaleTimeString([], {
     hour: "numeric",
     minute: "numeric",
     hour12: true,
   });
-
-  const currentDate = date.toLocaleDateString();
-
-  const [todoData, setTodoData] = useState(() => {
-    const storedTodo = localStorage.getItem(localKey);
-    return storedTodo ? JSON.parse(storedTodo) : [];
-  });
-
-  const [displayInput, setDisplayInput] = useState(false);
-  const userInputRef = useRef(null);
-
+  
   const generateId = () =>
     `${Date.now()}-${Math.random().toString(36).substr(2, 9)}`;
   const [editTodo, setEditTodo] = useState("");
 
-  const [editTodoValue, setEditTodoValue] = useState("");
-  const [filteredTodoData, setFilteredTodoData] = useState([]);
-  const [filterValue, setFilterValue] = useState("all");
-  const [toggleDarkMode, setToggleDarkMode] = useState("light");
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const [emptyTodoMessage, setEmptyTodoMessage] = useState(
-    "So peaceful... Add a task to bring some action 💡."
-  );
+  // const [todoData, setTodoData] = useState(() => {
+  //   const storedTodo = localStorage.getItem(localKey);
+  //   return storedTodo ? JSON.parse(storedTodo) : [];
+  // });
 
-  const [emptyCheckedMessage, setEmptyCheckedMessage] = useState(
-    "🕵️ Nothing to see here. Try changing the filter!"
-  );
+  // const [displayInput, setDisplayInput] = useState(false);
+
+  // const [editTodoValue, setEditTodoValue] = useState("");
+  // const [filteredTodoData, setFilteredTodoData] = useState([]);
+  // const [filterValue, setFilterValue] = useState("all");
+  // const [toggleDarkMode, setToggleDarkMode] = useState("light");
+
+  // const [emptyTodoMessage, setEmptyTodoMessage] = useState(
+  //   "So peaceful... Add a task to bring some action 💡."
+  // );
+
+  // const [emptyCheckedMessage, setEmptyCheckedMessage] = useState(
+  //   "🕵️ Nothing to see here. Try changing the filter!"
+  // );
 
   useEffect(() => {
     localStorage.setItem(localKey, JSON.stringify(todoData));
 
     const filtered =
-      filterValue === "checked"
-        ? todoData.filter((todo) => todo.checked)
-        : todoData;
+      state.filterValue === "checked"
+        ? state.todoData.filter((todo) => todo.checked)
+        : state.todoData;
 
-    setFilteredTodoData(filtered);
-  }, [todoData, filterValue]);
+    reducer("filter", filtered);
+  }, [state.todoData, state.filterValue]);
 
   return (
     <TodoContext.Provider
